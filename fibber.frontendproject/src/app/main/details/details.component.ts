1e98dd382dfe6667752ed6ef7a9037126faafb67
Abdelhady-169 <71092144+Abdelhady-169@users.noreply.github.com>
2021-09-24 17:09:37 -0700

    Add files via upload

---
+++ fibber.frontendproject/src/app/main/details/details.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MoviePassing } from 'src/app/movie.passing';
+import { Movie } from '../catalog-list/movie/movie';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent implements OnInit {
+  selectedmovie!: Movie;
+
+  constructor(private router: Router, public moviepassing: MoviePassing) { }
+
+  goToPage(){
+    this.router.navigate(['/main']);
+  }
+
+  ngOnInit(): void {
+    this.selectedmovie = this.moviepassing.getDetails();
+  }
+
+}
